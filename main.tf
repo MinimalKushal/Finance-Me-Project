e72b45770fee8f3bdac77a9d5d81c4516634cb98
MinimalKushal <120809383+MinimalKushal@users.noreply.github.com>
2023-04-18 10:20:04 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,24 @@
+resource "aws_instance" "Finance-Me-Deploy" {
+  ami           = "ami-07d3a50bd29811cd1" 
+  instance_type = "t2.micro" 
+  key_name = "Kushal"
+  vpc_security_group_ids= ["sg-0906b8b9eb805dc82"]
+  connection {
+    type     = "ssh"
+    user     = "ec2-user"
+    private_key = file("Kushal.pem")
+    host     = self.public_ip
+  }
+  provisioner "remote-exec" {
+    inline = [ "echo 'wait to start instance' "]
+  }
+  tags = {
+    Name = "Finance-Me-Deploy"
+  }
+  provisioner "local-exec" {
+        command = " echo ${aws_instance.Finance-Me-Deploy.public_ip} > inventory "
+  }
+   provisioner "local-exec" {
+  command = "ansible-playbook /var/lib/jenkins/workspace/FinanceMeProject/Playbook.yml "
+  } 
+}
